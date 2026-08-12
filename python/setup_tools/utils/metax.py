11e9e3415c5d2f0f986b8acf500ebeb51f810ce4
Stardep <146844969+Stardep-lmc@users.noreply.github.com>
2026-08-12 20:37:13 +0800

    [SPEC][METAX] Unify Python backend specialization (#962)
    ---------
    
    Co-authored-by: flagtree-bot <flagtree_ai@163.com>
    Co-authored-by: zhzhcookie <zhengyang@baai.ac.cn>

--- python/setup_tools/utils/metax.py
+++ python/setup_tools/utils/metax.py
@@ -19,8 +19,6 @@
 # SOFTWARE.
 
 import os
-import shutil
-from pathlib import Path
 
 
 def register_cache(cache, flagtree_backend, check_env, set_llvm_env):
@@ -42,12 +40,4 @@ def register_cache(cache, flagtree_backend, check_env, set_llvm_env):
 
 
 def install_extension(*args, **kargs):
-    # Copy knobs.py
-    _python_dir = Path(__file__).parent.parent.parent
-    src_triton_dir = _python_dir / "../third_party/metax/python/triton"
-    dst_triton_dir = _python_dir / "triton"
-
-    # copy runtime/driver.py
-    shutil.copy(src_triton_dir / "runtime/driver.py", dst_triton_dir / "runtime/driver.py")
-    # copy language/core.py semantic.py target_info.py
-    shutil.copytree(src_triton_dir / "language", dst_triton_dir / "language", dirs_exist_ok=True)
+    return
